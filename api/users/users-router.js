b3d78454555bd162d37226d3ea88b339fa1b11a8
Rodgers31 <otienor986@gmail.com>
2021-09-01 17:42:36 -0400

    All wired

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,4 +1,13 @@
 const express = require('express');
+const {
+	logger,
+	validateUserId,
+	validateUser,
+	validatePost,
+} = require('../middleware/middleware');
+
+const Users = require('./users-model');
+const Posts = require('../posts/posts-model');
 
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
@@ -6,39 +15,41 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // RETURN AN ARRAY WITH ALL THE USERS
+	// RETURN AN ARRAY WITH ALL THE USERS
 });
 
 router.get('/:id', (req, res) => {
-  // RETURN THE USER OBJECT
-  // this needs a middleware to verify user id
+	// RETURN THE USER OBJECT
+	// this needs a middleware to verify user id
 });
 
 router.post('/', (req, res) => {
-  // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
+	// RETURN THE NEWLY CREATED USER OBJECT
+	// this needs a middleware to check that the request body is valid
 });
 
 router.put('/:id', (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
+	// RETURN THE FRESHLY UPDATED USER OBJECT
+	// this needs a middleware to verify user id
+	// and another middleware to check that the request body is valid
 });
 
 router.delete('/:id', (req, res) => {
-  // RETURN THE FRESHLY DELETED USER OBJECT
-  // this needs a middleware to verify user id
+	// RETURN THE FRESHLY DELETED USER OBJECT
+	// this needs a middleware to verify user id
 });
 
 router.get('/:id/posts', (req, res) => {
-  // RETURN THE ARRAY OF USER POSTS
-  // this needs a middleware to verify user id
+	// RETURN THE ARRAY OF USER POSTS
+	// this needs a middleware to verify user id
 });
 
 router.post('/:id/posts', (req, res) => {
-  // RETURN THE NEWLY CREATED USER POST
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
+	// RETURN THE NEWLY CREATED USER POST
+	// this needs a middleware to verify user id
+	// and another middleware to check that the request body is valid
 });
+
+module.exports = router;
 
 // do not forget to export the router
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // require your server and launch it
+
+const server = require('./api/server');
+
+const port = 5000;
+
+server.listen(port, () => {
+	console.log(`Server running on http://localhost:${port}`);
+});
